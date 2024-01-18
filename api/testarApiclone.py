737c3f6fcf6f15fd628659aa355504a6d904a867
Cristiano Gregory Monfrim Camandaroba <88840308+Camandaroba06@users.noreply.github.com>
2024-01-17 22:11:26 -0300

    Add files via upload

--- api/testarApiclone.py
+++ api/testarApiclone.py
@@ -5,16 +5,39 @@ import joblib
 import tensorflow as tf
 from keras import backend as K
 import pandas as pd
+import os
+import random
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras import layers
+from sklearn.model_selection import train_test_split
 
 app = Flask(__name__)
 
 # Carregar o modelo treinado a partir do arquivo .h5
-model = load_model("modelo_seed42.h5")
-scaler_carregado = joblib.load("meu_scaler_minmax.joblib")
+SEED = 215
+
+
+def set_seeds(seed=SEED):
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    random.seed(seed)
+    tf.random.set_seed(seed)
+    np.random.seed(seed)
+
+
+def set_global_determinism(seed=SEED):
+    set_seeds(seed=seed)
+
+    os.environ["TF_DETERMINISTIC_OPS"] = "1"
+    os.environ["TF_CUDNN_DETERMINISTIC"] = "1"
+
+    tf.config.threading.set_inter_op_parallelism_threads(1)
+    tf.config.threading.set_intra_op_parallelism_threads(1)
 
 
 @app.route("/previsao", methods=["POST"])
 def prever():
+    model = load_model("modelo_seed42.h5")
+    scaler_carregado = joblib.load("meu_scaler_minmax.joblib")
     # Receber os dados da requisição em formato JSON
     dados_entrada = request.get_json()
 
@@ -42,65 +65,95 @@ def prever():
 
 @app.route("/retreinamento", methods=["POST"])
 def retreinar():
-    global model
-    # Ler o último valor de loss do arquivo
-    with open("ultimo_loss.txt", "r") as file:
-        ultimo_loss_lido = float(file.read())
-
-    print(f"Último valor de loss lido: {ultimo_loss_lido}")
-
-    dados_json = request.get_json()
-
-    data = np.array(dados_json["valores"]).reshape(-1, 1)
-    normalized_data = scaler_carregado.transform(data)
-    Data_Power = pd.Series(
-        [value for row in normalized_data for value in row],
-        name="Daily Power yields (kWh)",
+    count = 0
+    set_global_determinism(seed=SEED)
+    data = pd.read_csv(
+        "dados_diarios_14Nov_outliers.csv"
+    )  # CSV q representa a requisição ao servidor dos dados todos
+    data = data.rename(
+        columns={"Daily Power yields (kWh)": "energy", "inverterdatetime": "time"}
     )
-    print(Data_Power)
+    data_power = data["energy"]
+    data_power = np.array(data_power)
+    scaler = MinMaxScaler()
+    data_power = data_power.reshape(-1, 1)
+    normalized_data = scaler.fit_transform(data_power)
+
+    data_power = pd.Series(
+        [value for row in normalized_data for value in row], name="energy"
+    )
     X = pd.concat(
         [
-            Data_Power.shift(1),
-            Data_Power.shift(2),
-            Data_Power.shift(3),
+            data_power.shift(1),
+            data_power.shift(2),
+            data_power.shift(3),
         ],
         axis=1,
     )
-    y = pd.concat([Data_Power.shift(-3)], axis=1)
+    y = pd.concat([data_power.shift(-3)], axis=1)
     X.dropna(inplace=True)
-    y.dropna(subset=["Daily Power yields (kWh)"], inplace=True)
-    print(X.shape)
-    print(y.shape)
+    y.dropna(subset=["energy"], inplace=True)
     X = X.to_numpy()
     y = y.to_numpy()
     y = y.flatten()
-    print(X)
-    print(y)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, shuffle=False, stratify=None
+    )
+    model = tf.keras.models.load_model("modelo_australia.h5")
+    model.trainable = False
+    print(model.summary())
+    for layer in model.layers:
+        layer.trainable = False
+    base_inputs = model.layers[0].input
+    base_outputs = model.layers[-2].output
 
-    # Compilar o modelo
-    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-    model.compile(optimizer=optimizer, loss="mean_squared_error")
+    output = layers.Dense(1, name="output")(base_outputs)
+    new_model = tf.keras.Model(inputs=base_inputs, outputs=output)
+    new_model.summary()
+    new_model.compile(
+        optimizer="adam", loss="mean_squared_error"
+    )  # Note que estou usando o "adam" logo ta com lr no default
 
-    # Treinar o modelo com os novos dados
-    history = model.fit(X, y, epochs=10, batch_size=32)
+    history = new_model.fit(
+        X_train, y_train, epochs=20, batch_size=32, validation_split=0.3
+    )
+
+    predicoes = new_model.predict(X_test)
+
+    predicoes = scaler.inverse_transform(predicoes)
+
+    real = scaler.inverse_transform(y_test.reshape(-1, 1))
+
+    erro = np.mean(((predicoes - real) ** 2) ** 0.5)
+
+    if count == 0:
+        ultimo_loss_lido = 130
 
     # Salvar o modelo treinado
-    if history.history["loss"][-1] < ultimo_loss_lido:
-        model.save("seu_modelo_retreinado_API.h5")
+    if erro < ultimo_loss_lido:
+        new_model.save("seu_modelo_retreinado_API.h5")
+        ultimo_loss_lido = erro
         return jsonify(
             {
-                "mensagem": "Modelo retreinado com sucesso e o erro caiu em relação ao anterior e será atualizado no BD",
-                "valor_Loss_anterior": ultimo_loss_lido,
-                "valor_Loss_retreinamento": history.history["loss"][-1],
+                "Ribeirão": {
+                    "mensagem": "Modelo retreinado com sucesso e o erro caiu em relação ao anterior e será atualizado no BD",
+                    "valor_Loss_anterior": ultimo_loss_lido,
+                    "valor_Loss_retreinamento": history.history["loss"][-1],
+                    "erro": erro,
+                }
             }
         )
     else:
-        model = load_model("modelo_seed42.h5")
+        model.save("seu_modelo_retreinado_API.h5")
+        ultimo_loss_lido = erro
         return jsonify(
             {
-                "mensagem": "Modelo retreinado com sucesso, mas o erro não caiu em relação ao anterior. Logo, será mantido o anterior no BD",
-                "valor_Loss_anterior": ultimo_loss_lido,
-                "valor_Loss_retreinamento": history.history["loss"][-1],
+                "Ribeirão": {
+                    "mensagem": "Modelo retreinado com sucesso, mas o erro não caiu em relação ao anterior. Logo, será mantido o anterior no BD",
+                    "valor_Loss_anterior": ultimo_loss_lido,
+                    "valor_Loss_retreinamento": history.history["loss"][-1],
+                    "erro": erro,
+                }
             }
         )
 
